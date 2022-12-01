2c831a118a546c311b4ae781505319fa75db4fdd
IagoFloriano <iagomfloriano@gmail.com>
2022-12-01 15:51:43 -0300

    q5 q6

---
+++ T1/q5_qtd_encomendas.sql
@@ -0,0 +1,6 @@
+select n.n_name, count(o.o_orderkey) as total
+from nation n, customer c, orders o
+where n.n_nationkey = c.c_nationkey and
+c.c_custkey = o.o_custkey
+group by n.n_nationkey
+order by total desc

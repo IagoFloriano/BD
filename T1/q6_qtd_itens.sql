689855d4198cc81a91d58e74a14d02c9528e7df4
Iago Floriano <iagomfloriano@gmail.com>
2022-12-08 17:32:58 -0300

    q6 e q7 agr certos

--- T1/q6_qtd_itens.sql
+++ T1/q6_qtd_itens.sql
@@ -1,4 +1,4 @@
-select n.n_name, count(l.l_partkey) as total
+select n.n_name, sum(l.l_quantity) as total
 from nation n, customer c, orders o, lineitem l
 where n.n_nationkey = c.c_nationkey and
 c.c_custkey = o.o_custkey and
